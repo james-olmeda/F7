e0bf7686715cc574c765846554e78a2ec818dbf4
james-olmeda <jamesolmeda17@gmail.com>
2026-03-01 20:31:24 -0400

    Initial Commit

---
+++ F7/F7App.swift
@@ -0,0 +1,17 @@
+//
+//  F7App.swift
+//  F7
+//
+//  Created by James on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct F7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
